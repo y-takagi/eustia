1f5a2e7736f9af23c11ab010718245cd5e09290c
y-takagi <ytakagi.deb@gmail.com>
2016-10-28 01:41:05 +0900

    Rename project

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-projects=(`ls -d -- projects/*/`)
+projects=(`ls -d -- digdag_projects/*/`)
 
 for project in ${projects[@]}; do
     digdag push `basename $project` --project $project -e $DIGDAG_ENDPOINT
